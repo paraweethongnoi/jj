ee023b57587abeca992c7160de2d7f4103ded46b
paraweethongnoi <parawee191@gmail.com>
2021-01-29 22:11:42 +0700

    c

--- lib/page/history/history_pag5.dart
+++ lib/page/history/history_pag5.dart
@@ -19,23 +19,217 @@ class _HistoryPag5State extends State<HistoryPag5> {
             icon: Icon(Icons.arrow_back),
           ),
           title: Text('ทำเนียบผู้บริหาร')),
-      body: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        child: Container(
-          child: ListTile(
-            leading: Icon(Icons.people),
-            title: Text(
-              '''เอ.ซี.คาร์เตอร์
+      body: ListView(children: <Widget>[
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''เอ.ซี.คาร์เตอร์
 พ.ศ.๒๔๓๙ – ๒๔๔๖
 ''',
-              style: TextStyle(fontSize: 22.0, color: Colors.purple),
-            ),
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
           ),
-          
         ),
-        
-      ),
-      
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''หลวงบำเหน็จวรญาณ
+พ.ศ.๒๔๔๖ – ๒๔๔๗
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''ขุนวิเทศดรุณการ
+พ.ศ.๒๔๔๘ – ๒๔๕๘
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''พระประมวลวิชาพูล
+พ.ศ.๒๔๕๘ – ๒๔๗๐
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''พระปวโรฬาร
+พ.ศ.๒๔๗๐ – ๒๔๗๒
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''พระยาวิเศษศุภวัตร์
+พ.ศ.๒๔๗๓ – ๒๔๘๐
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''หลวงทรงวิทยาศาสตร์
+พ.ศ.๒๔๘๑ – ๒๔๘๒
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''หลวงภารสาร
+พ.ศ.๒๔๘๓ – ๒๔๙๑
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''นายจรูญ วงศ์สายัณห์
+พ.ศ.๒๔๙๑ – ๒๔๙๙
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''หลวงบุญปาลิตวิชาสาสก์
+พ.ศ.๒๔๙๙ – ๒๕๐๐
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''นายสวัสดิ์ สิงหพงษ์
+พ.ศ.๒๕๐๐ – ๒๕๐๑
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''นายโชค สุคันธวณิช
+พ.ศ.๒๕๐๑ – ๒๕๐๔
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''นายพร ทองพูนศักดิ์
+พ.ศ.๒๕๐๔ – ๒๕๐๘
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''นายศิริ ศุขกิจ
+พ.ศ.๒๕๐๘ – ๒๕๑๑
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''นายจรูญ มิลินทร์
+พ.ศ.๒๕๑๑ – ๒๕๑๘
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''ม.ร.ว.ทวีโภค เกษมศรี
+พ.ศ.๒๕๑๘ – ๒๕๒๑
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''นายบุญเลิศ ศรีหงส์
+พ.ศ.๒๕๒๑ – ๒๕๒๘
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''รศ.สุรพันธ์ ยันต์ทอง
+พ.ศ.๒๕๒๘ – ๒๕๓๒
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''รศ.ฉลอง ภิรมย์รัตน์
+พ.ศ.๒๕๓๒ – ๒๕๓๖
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''รศ.สันต์ ธรรมบำรุง
+พ.ศ.๒๕๓๖ – ๒๕๔๖
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''รศ.ดร.สุพล วุฒิเสน
+พ.ศ.๒๕๔๖ – ๒๕๕๕
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                        ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''รศ.ดร.พลสิทธิ์ หนูชูชัย
+พ.ศ.๒๕๕๕ – ๒๕๕๖
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+                                ListTile(
+          leading: Icon(Icons.people),
+          title: Text(
+            '''ผศ.ดร.ลินดา เกณฑ์มา
+๒๙ ตุลาคม ๒๕๕๘ – ปัจจุบัน
+''',
+            style: TextStyle(fontSize: 22.0, color: Colors.purple),
+          ),
+        ),
+
+
+      ]),
     );
   }
 }
